8d51b8c34848ce0cfb39c5a6d5ecc1c270fd27e4
william341 <wwokwilliam@gmail.com>
2016-07-31 14:20:23 -0700

    move server

--- Toaster/Toaster/Program.cs
+++ Toaster/Toaster/Program.cs
@@ -1,15 +1,54 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Toaster
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
+
+            while (true)
+            {
+                try
+                {
+                    IPAddress ipAd = IPAddress.Parse("0.0.0.0");
+                    // use local m/c IP address, and 
+                    // use the same in the client
+
+                    /* Initializes the Listener */
+                    TcpListener myList = new TcpListener(ipAd, 13370);
+
+                    /* Start Listeneting at the specified port */
+                    myList.Start();
+
+                    Console.WriteLine("The local End point is: " +
+                                      myList.LocalEndpoint);
+                    Console.WriteLine("Waiting for a connection...");
+
+                    Socket s = myList.AcceptSocket();
+                    Console.WriteLine("Connection accepted from " + s.RemoteEndPoint);
+                    ASCIIEncoding asen = new ASCIIEncoding();
+
+                    s.Send(File.ReadAllBytes("default.md"));
+                    s.Send(new byte[] { 4 });
+
+                    /* clean up */
+                    s.Close();
+                    myList.Stop();
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Error! " + e.StackTrace);
+                }
+            }
         }
     }
 }
